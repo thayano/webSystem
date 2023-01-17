8b0f367fd8d88eca4fa97ab8f1647ed8bc333541
thayano.davalo <thayano.davalo@nastek.com.br>
2023-01-17 17:44:22 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import {close, logo, menu} from '../assets';
+import {navLinks} from '../constants';
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="w-full flex py-6 justfy-between items-center navbar">
+      <img src={logo} alt="logo" className='w-[136px] h-[32px]'/>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav,index) => (
+          <li key={nav.id}
+          className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'} text-white`}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img src={toggle ? close : menu}/>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
